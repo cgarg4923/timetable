30126ba6ba5dcea9f2d2c56aab928839131ea0a3
cgarg4923 <cgarg4923@gmail.com>
2022-01-16 13:40:34 +0530

    Info Item Style Changed

--- lib/widgets/info_item.dart
+++ lib/widgets/info_item.dart
@@ -31,92 +31,122 @@ class _InfoItemState extends State<InfoItem> {
   @override
   Widget build(BuildContext context) {
     final _info = Provider.of<Info>(context, listen: false);
+    Future<bool> showConfirmationDialogBox() {
+      return showDialog(
+        context: context,
+        builder: (_) => AlertDialog(
+          title: Text('Are You Sure?'),
+          content: Text('Do you want to remove item from the cart?'),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+              child: Text('No'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _info.delete(widget.day, widget.id);
+                Navigator.of(context).pop(true);
+              },
+              child: Text('Yes'),
+            ),
+          ],
+        ),
+      );
+    }
 
     return Card(
       margin: EdgeInsets.all(8),
-      child: Column(
-        children: [
-          ListTile(
-            leading: Text(
-              MaterialLocalizations.of(context)
-                  .formatTimeOfDay(widget.startTime),
-            ),
-            title: Text(widget.title),
-            onTap: () {
-              setState(() {
-                _expanded = !_expanded;
-              });
-            },
+      child: Dismissible(
+        key: ValueKey(widget.id),
+        background: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(6),
+            color: Theme.of(context).errorColor,
           ),
-          if (_expanded)
-            Container(
-              height: 100,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ListView(
-                  children: [
-                    Text(
-                      MaterialLocalizations.of(context)
-                              .formatTimeOfDay(widget.startTime) +
-                          ' - ' +
-                          MaterialLocalizations.of(context)
-                              .formatTimeOfDay(widget.endTime),
-                    ),
-                    Text(
-                      '${widget.info}',
-                      softWrap: true,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        ElevatedButton(
-                          onPressed: () {
-                            Navigator.pushNamed(
-                              context,
-                              EditItem.routeName,
-                              arguments: [
-                                widget.day,
-                                widget.id,
-                              ],
-                            );
-                          },
-                          child: Text('Edit'),
-                        ),
-                        ElevatedButton(
-                          onPressed: () {
-                            showDialog(
-                              context: context,
-                              builder: (_) => AlertDialog(
-                                title: Text('Are You Sure?'),
-                                content: Text(
-                                    'Do you want to remove item from the cart?'),
-                                actions: [
-                                  ElevatedButton(
-                                    onPressed: () {
-                                      Navigator.of(context).pop();
-                                    },
-                                    child: Text('No'),
-                                  ),
-                                  ElevatedButton(
-                                    onPressed: () {
-                                      _info.delete(widget.day, widget.id);
-                                      Navigator.of(context).pop();
-                                    },
-                                    child: Text('Yes'),
-                                  ),
-                                ],
-                              ),
-                            );
-                          },
-                          child: Text('Delete'),
-                        ),
-                      ],
-                    ),
-                  ],
+          child: Icon(
+            Icons.delete,
+            color: Colors.white,
+            size: 40,
+          ),
+          alignment: Alignment.centerRight,
+          padding: EdgeInsets.only(right: 20),
+          margin: EdgeInsets.symmetric(
+            horizontal: 15,
+            vertical: 4,
+          ),
+        ),
+        direction: DismissDirection.endToStart,
+        confirmDismiss: (direction) {
+          return showConfirmationDialogBox();
+        },
+        onDismissed: (direction) {
+          _info.delete(widget.day, widget.id);
+        },
+        child: Column(
+          children: [
+            ListTile(
+              leading: Text(
+                MaterialLocalizations.of(context)
+                    .formatTimeOfDay(widget.startTime),
+              ),
+              title: Text(
+                widget.title,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
                 ),
               ),
+              onTap: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
+              trailing: IconButton(
+                icon: Icon(Icons.edit),
+                splashColor: Theme.of(context).primaryColor,
+                onPressed: () {
+                  Navigator.pushNamed(
+                    context,
+                    EditItem.routeName,
+                    arguments: [
+                      widget.day,
+                      widget.id,
+                    ],
+                  );
+                },
+              ),
             ),
-        ],
+            if (_expanded)
+              Container(
+                height: 70,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListView(
+                    children: [
+                      Text(
+                        MaterialLocalizations.of(context)
+                                .formatTimeOfDay(widget.startTime) +
+                            ' - ' +
+                            MaterialLocalizations.of(context)
+                                .formatTimeOfDay(widget.endTime),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        '${widget.info}',
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                        softWrap: true,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+          ],
+        ),
       ),
     );
   }
